c92ee71ee675b593de9283546b61d8261a558ee5
juandabi <jdavidcardonam@gmail.com>
2021-11-15 18:37:13 -0500

    Borrar todo y volver a empezar

--- BackendEDUFREE/src/repositories/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './administrador.repository';
-export * from './asignatura.repository';
-export * from './calificacion.repository';
-export * from './docente.repository';
-export * from './estudiante.repository';
-export * from './grupo.repository';
-export * from './programa-institucional.repository';
